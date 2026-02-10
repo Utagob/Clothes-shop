67e47ca956e9d10d801f930bb06835064c585ce9
Alexei <alexeibogatu@gmail.com>
2026-02-10 11:16:31 +0200

    add all site wihtout footer with JS

--- content.js
+++ content.js
@@ -1,23 +1,79 @@
-const head = document.getElementsByTagName("header")[0];
+const body = document.getElementsByTagName('body')[0];
+const cd = document.createElement('button');
+cd.setAttribute("id", "cb");
+cd.innerText = "Share";
+body.appendChild(cd);
+const head = document.createElement('header');
 const content = {
     title: "Phórema",
-    imageLink: [
+    slogan: "Anything you can think about is here",
+    headImage: [
         "image/icon.png",
         "image/icon-n.png",
         "image/acc.png",
         "image/acc-n.png",
         "M480-120q-150 0-255-105T120-480q0-150 105-255t255-105q14 0 27.5 1t26.5 3q-41 29-65.5 75.5T444-660q0 90 63 153t153 63q55 0 101-24.5t75-65.5q2 13 3 26.5t1 27.5q0 150-105 255T480-120Z",
         "M480-280q-83 0-141.5-58.5T280-480q0-83 58.5-141.5T480-680q83 0 141.5 58.5T680-480q0 83-58.5 141.5T480-280ZM200-440H40v-80h160v80Zm720 0H760v-80h160v80ZM440-760v-160h80v160h-80Zm0 720v-160h80v160h-80ZM256-650l-101-97 57-59 96 100-52 56Zm492 496-97-101 53-55 101 97-57 59Zm-98-550 97-101 59 57-100 96-56-52ZM154-212l101-97 55 53-97 101-59-57Z",
+    ],
+    mainImage:[
+        "image/st.png",
+        "image/st1.png",
+        "image/st2.png",
+    ],
+    showU:[
+        `<div class="prod">
+          <p>Sweaters</p>
+          <img src="image/exm/sw.png">
+        </div>
+        <div class="prod">
+          <p>T-shirts</p>
+          <img src="image/exm/sh.png">
+        </div>
+        <div class="prod">
+          <p>Hoodies</p>
+          <img src="image/exm/hood.png">
+        </div>`,
+        `<div class="prod">
+          <p>Jackets</p>
+          <img src="image/exm/ja.png">
+        </div>
+        <div class="prod">
+          <p>Coats</p>
+          <img src="image/exm/co.png">
+        </div>`,
+    ],
+    showL:[
+        `<div class="prod">
+          <p>Pants</p>
+          <img src="image/exm/pa.png">
+        </div>
+        <div class="prod">
+          <p>Shorts</p>
+          <img src="image/exm/so.png">
+        </div>`,
+        `<div class="prod">
+          <p>Sneakers</p>
+          <img src="image/exm/sn.png">
+        </div>
+        <div class="prod">
+          <p>Boots</p>
+          <img src="image/exm/boots.png">
+        </div>
+        <div class="prod">
+          <p>Classic Shoes</p>
+          <img src="image/exm/csh.png">
+        </div>`
     ]
+
 }
 
 const s = document.createElement('div');
     s.setAttribute("class", "s");
 const i1 = document.createElement('img');
-    i1.setAttribute("src", content.imageLink[0]);
+    i1.setAttribute("src", content.headImage[0]);
     i1.setAttribute("id", "iS");
 const i2 = document.createElement('img');
-    i2.setAttribute("src", content.imageLink[1]);
+    i2.setAttribute("src", content.headImage[1]);
     i2.setAttribute("id", "iN");
 const title = document.createElement("h1");
     title.innerText = content.title;
@@ -31,13 +87,13 @@ const r = document.createElement('div');
 const b1 = document.createElement("button");
     b1.setAttribute("id", "acS");
 const i3 = document.createElement("img");
-    i3.setAttribute("src", content.imageLink[2]);
+    i3.setAttribute("src", content.headImage[2]);
 b1.appendChild(i3);
 
 const b2 = document.createElement("button");
     b2.setAttribute("id", "acN");
 const i4 = document.createElement("img");
-    i4.setAttribute("src", content.imageLink[3]);
+    i4.setAttribute("src", content.headImage[3]);
 b2.appendChild(i4);
 
 const b3 = document.createElement("button");
@@ -57,14 +113,64 @@ function createIcon(pathData) {
     svg.appendChild(path);
     return svg;
 }
-const svg1 = createIcon(content.imageLink[4]);
-const svg2 = createIcon(content.imageLink[5]);
+const svg1 = createIcon(content.headImage[4]);
+const svg2 = createIcon(content.headImage[5]);
 
 b3.appendChild(svg1);
 b3.appendChild(svg2);
 r.appendChild(b1);
 r.appendChild(b2);
 r.appendChild(b3);
-
 head.appendChild(s);
 head.appendChild(r);
+body.appendChild(head);
+
+const div1 = document.createElement('div');
+div1.setAttribute("class", "nav");
+const p = document.createElement('p');
+const aW = document.createElement('a');
+const aM = document.createElement('a');
+aW.innerText = 'Women';
+aM.innerText = 'Men';
+p.innerText = '';
+div1.appendChild(aW);
+div1.appendChild(p);
+div1.appendChild(aM);
+
+const div2 = document.createElement('div');
+div2.setAttribute("class", "show");
+for(let i=0; i<content.mainImage.length; i++){
+    const imgM = document.createElement('img');
+    imgM.setAttribute("src", content.mainImage[i]);
+    div2.appendChild(imgM);
+}
+body.appendChild(div1);
+body.appendChild(div2);
+
+const pr = document.createElement('div');
+pr.setAttribute("class", "pr");
+const pq = document.createElement('p');
+pq.innerText = content.slogan;
+const pU = document.createElement('p');
+pU.innerText = "Upper Body";
+const pL = document.createElement('p');
+pL.innerText = "Lower Body";
+
+pr.appendChild(pq);
+pr.appendChild(pU);
+for(let i=0; i<content.showU.length; i++){
+    const sec = document.createElement('div');
+    sec.setAttribute("class", "sec");
+    sec.innerHTML = content.showU[i];
+    pr.appendChild(sec);
+}
+pr.appendChild(pL);
+for(let i=0; i<content.showL.length; i++){
+    const sec = document.createElement('div');
+    sec.setAttribute("class", "sec");
+    sec.innerHTML = content.showL[i];
+    pr.appendChild(sec);
+}
+
+
+body.appendChild(pr);
