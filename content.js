8688074dcca90ee6adfca699d33d3926ea75f658
Alexei <alexeibogatu@gmail.com>
2026-03-03 10:33:59 +0200

    save theme status in localStorage

--- content.js
+++ content.js
@@ -219,3 +219,12 @@ px.innerText = "©Copyright by Phorema";
 td.appendChild(px);
 footer.appendChild(td);
 body.appendChild(footer);
+
+
+
+const clothing = {
+    type: "",
+    name: "",
+    imageUrl: ""
+}
+const clothes = [];
--- scrypt.js
+++ scrypt.js
@@ -1,15 +1,29 @@
+window.addEventListener("load", ()=> {
+    const themeStored = localStorage.getItem('theme');
+    if(themeStored === 'dark'){
+            theme.setAttribute("href","dark-style.css");
+            b1.id = "acN";
+            i3.setAttribute("src", content.headImage[3]);
+        } else {
+            theme.setAttribute("href", "style.css");
+            b1.id = "acS";
+            i3.setAttribute("src", content.headImage[2]);
+        }
+})
+
 const theme = document.getElementById("theme");
 function changeTheme(){
     if(theme.getAttribute("href") === "style.css"){
         theme.setAttribute("href","dark-style.css");
         b1.id = "acN";
         i3.setAttribute("src", content.headImage[3]);
+        localStorage.setItem("theme", "dark");
     } else {
         theme.setAttribute("href", "style.css");
         b1.id = "acS";
         i3.setAttribute("src", content.headImage[2]);
+        localStorage.setItem("theme", "light");
     }
-    return 0;
 }
 
 const chBtn = document.getElementById("dark-ch");
@@ -19,7 +33,6 @@ const button = document.getElementById("cb");
 
 function consB(){
     console.log("No share :(");
-    return 0;
 }
 
 button.addEventListener("click", consB);
